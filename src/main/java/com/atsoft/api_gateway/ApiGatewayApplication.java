a4b6652ca3930c7268b7ce3fd365d9003a0cafdf
ahmed diallo <tidiane.mqp@gmail.com>
2025-07-26 04:08:59 +0200

    complete proxy

--- src/main/java/com/atsoft/api_gateway/ApiGatewayApplication.java
+++ src/main/java/com/atsoft/api_gateway/ApiGatewayApplication.java
@@ -2,6 +2,10 @@ package com.atsoft.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
@@ -9,5 +13,9 @@ public class ApiGatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
-
+	//configuration dynamique des routes
+	@Bean
+	DiscoveryClientRouteDefinitionLocator locator(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties dlp){
+		return	new DiscoveryClientRouteDefinitionLocator(rdc,dlp);
+	}
 }
